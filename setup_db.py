e46633cd3ba83e8ab2a79ea6c8542a52683126b8
jmcad <jmcadinez@gmail.com>
2021-05-20 22:35:26 +0200

    created playlist samples, implemented a create_table_playlists function

--- setup_db.py
+++ setup_db.py
@@ -9,8 +9,13 @@ music_data = [
                 (6, 'Royalty', 'Egzod, Maestro Chives, Neoni', 223, 'royalty.jpg', '/static/assets/Egzod, Maestro Chives, Neoni - Royalty [NCS Release].mp3'),
              ]
 
+playlist_samples = [
+                        (1, 'My Playlist', 'This is my personal playlist. It contains great music!'),
+                        (2, 'Cool Playlist', 'The coolest playlist ever!'),
+                    ]
+
 # CREATE TABLE
-def create_table(conn):
+def create_table_tracks(conn):
     cur = conn.cursor()
 
     sql = ("CREATE TABLE tracks ("
@@ -26,18 +31,39 @@ def create_table(conn):
     except sqlite3.Error as err:
         print(err)
     else:
-        print("Table created!")
+        print("Tracks table created!")
     finally:
         cur.close()
+
+def create_table_playlists(conn):
+    cur = conn.cursor()
+
+    sql = ("CREATE TABLE playlists ("
+                "playlist_id INTEGER, "
+                "name VARCHAR(20), "
+                "description VARCHAR(50), "
+                "PRIMARY KEY(playlist_id))")
+    
+    try:
+        cur.execute(sql)
+    except sqlite3.Error as err:
+        print(err)
+    else:
+        print("Playlists table created!")
+    finally:
+        cur.close()
+
 
 # DROP TABLE
 def drop_table(conn):
     cur = conn.cursor()
 
     sql = "DROP TABLE tracks"
+    sql2 = "DROP TABLE playlists"
 
     try:
         cur.execute(sql)
+        cur.execute(sql2)
     except sqlite3.Error as err:
         print(err)
     else:
@@ -45,14 +71,17 @@ def drop_table(conn):
     finally:
         cur.close()
 
+
 # INSERT DATA TO TABLE
 def insert_data(conn):
     cur = conn.cursor()
 
-    sql = "INSERT INTO tracks VALUES (?,?,?,?,?,?)"
+    sqlTracks = "INSERT INTO tracks VALUES (?,?,?,?,?,?)"
+    sqlPlaylists = "INSERT INTO playlists VALUES (?,?,?)"
 
     try:
-        cur.executemany(sql, music_data)
+        cur.executemany(sqlTracks, music_data)
+        cur.executemany(sqlPlaylists, playlist_samples)
         conn.commit()
         print("Data inserted to table!")
     except sqlite3.Error as err:
@@ -81,7 +110,7 @@ if __name__ == "__main__":
         print(err)
     else:
         drop_table(conn)
-        create_table(conn)
+        create_table_tracks(conn)
+        create_table_playlists(conn)
         insert_data(conn)
-        # query_data(conn)
         conn.close()
